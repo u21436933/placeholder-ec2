210bcc71ed55ae78a82a01d653c671bd1d524e5b
Troy C <u21436933@tuks.co.za>
2024-07-31 22:20:20 +0200

    Init Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo512.png";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+
+				<h1>Lookout Under Construction</h1>
+			</header>
+		</div>
+	);
+}
+
+export default App;
